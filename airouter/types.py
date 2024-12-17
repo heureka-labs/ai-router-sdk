342a4db72594b4c88f6fe69d0ccf050bb2a0f00b
Matthias Lau <lau.matthias@gmail.com>
2024-12-17 16:19:07 +0100

    Remove specific model versions to harden the sdk

--- airouter/types.py
+++ airouter/types.py
@@ -2,18 +2,18 @@ from enum import Enum
 
 
 class Model(Enum):
-    GPT_4O = 'gpt-4o-2024-08-06'
-    GPT_4O_MINI = 'gpt-4o-mini-2024-07-18'
-    MISTRAL_LARGE = 'mistral-large-2407'
+    GPT_4O = 'gpt-4o'
+    GPT_4O_MINI = 'gpt-4o-mini'
+    MISTRAL_LARGE = 'mistral-large'
     LLAMA_31_8B = 'llama-3.1-8b'
     LLAMA_31_70B = 'llama-3.1-70b'
     LLAMA_31_405B = 'llama-3.1-405b'
-    CLAUDE_3_HAIKU = 'claude-3-haiku-20240307'
-    CLAUDE_3_OPUS = 'claude-3-opus-20240229'
-    CLAUDE_35_SONNET = 'claude-3-5-sonnet-20241022'
+    CLAUDE_3_HAIKU = 'claude-3-haiku'
+    CLAUDE_3_OPUS = 'claude-3-opus'
+    CLAUDE_35_SONNET = 'claude-3-5-sonnet'
     COMMAND_R_PLUS = 'command-r-plus'
-    GEMINI_15_FLASH = 'gemini-1.5-flash-001'
-    GEMINI_15_PRO = 'gemini-1.5-pro-001'
+    GEMINI_15_FLASH = 'gemini-1.5-flash'
+    GEMINI_15_PRO = 'gemini-1.5-pro'
     QWEN25_72B = 'qwen2.5-72B'
     UNKNOWN = 'unknown'
 
